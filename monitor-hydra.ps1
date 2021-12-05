d013df9420a127c90b070bf7bcd5dfd153be8739
John Moreira <johnmoreira1019@gmail.com>
2021-12-05 10:43:45 -0500

    Update monitor-hydra.ps1

--- monitor-hydra.ps1
+++ monitor-hydra.ps1
@@ -1,12 +1,12 @@
 #$stopWatch = [system.diagnostics.stopwatch]::startNew()
     $latestLog = gci "C:\Users\john\Desktop\HYDRA 1.0E PRO\LOGS" | sort LastWriteTime | select -last 1
     #$latestLog = "C:\Users\john\Desktop\HYDRA 1.0E PRO\LOGS\" + $latestLog
-    write-host $latestLog
+    #write-host $latestLog
     $logContent = Get-Content $latestLog
-    Write-Host $logContent
+    #Write-Host $logContent
     if ($logContent -like "*CMD_FAILED*")
     {
-        Write-Host "The HYDRA process has died. Kill the HYDRA process"
+        #Write-Host "The HYDRA process has died. Kill the HYDRA process"
         Stop-Process -Name "HYDRA" -Force
         #Start-Sleep 2
         #Start-Process -FilePath "C:\Users\john\Desktop\HYDRA 1.0E PRO\HYDRA.EXE"
@@ -14,7 +14,7 @@
     }
     else 
     {
-        Write-Host "HYDRA has not encountered a fatal error"    
+        #Write-Host "HYDRA has not encountered a fatal error"    
         #Invoke-Item "C:\Users\john\Desktop\HYDRA 1.0E PRO\HYDRA.EXE"
         #Start-Process -FilePath "C:\Users\john\Desktop\HYDRA 1.0E PRO\HYDRA.EXE"
         #taskkill /IM HYDRA.exe /T /F
